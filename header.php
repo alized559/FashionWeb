c749ba871d3a4171f1d378c7ae1cd08d47a6429e
Toxic Scams <toxicscams@gmail.com>
2022-04-26 17:03:26 +0300

    v1.0

---
+++ header.php
@@ -0,0 +1,32 @@
+<link href="css/header.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<div class="navbar-head">
+    <div class="imgcontainer">
+        <img src="imgs/logo.png">
+    </div>
+    <div class="rightcontainer">
+        <div class="btn-group">
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="imgs/flags/us.png">
+                US, USD
+            </button>
+
+            <ul class="dropdown-menu">
+                <li class="dropdown-header">Choose A Currency</li>
+                <li>
+                    <a href="#" title="Select this card"><img src="imgs/flags/us.png">US, USD</a>
+                </li>
+                <li>
+                    <a href="#" title="Select this card"><img src="imgs/flags/lb.png">LB, LBP</a>
+                </li>
+            </ul>
+        </div>
+
+        <a href="help.html">Help</a>
+        <div class="vl"></div>
+        <img src="imgs/user_logo.png">
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Fashion | Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/main.css" rel="stylesheet" media="screen">
+    <link rel="icon" href="imgs/favicon.ico" type="image/x-icon">
+    <?php include "header.php"; ?>
+</head>
+<body>
+    
+</body>
+</html>
