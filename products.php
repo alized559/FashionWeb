52445ca2a00f97db384d9d4a2f8d824e093a5ad6
Ali Zein Al Dine <alized559@gmail.com>
2022-04-27 16:21:03 +0300

    add product-list, filter on mobile

--- products.php
+++ products.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fashion | Products</title>
+    <link href="css/products.css" rel="stylesheet" media="screen">
 
     <?php include('header.php') ?>
-    <link href="css/products.css" rel="stylesheet" media="screen">
 </head>
 <body>
     <div class="flexbox">
-        <div class="filter">
+        <div class="filter" id="desktop">
             <form>
                 <div class="filter-header1">
                     <a onclick="controlFilter('collapseFilter1')" style="text-decoration: none;" data-toggle="collapse" href="#collapseFilter1">
@@ -50,7 +50,7 @@
 
                 <div class="filter-header">
                     <a onclick="controlFilter('collapseFilter3')" style="text-decoration: none;" data-toggle="collapse" href="#collapseFilter3">
-                        <i id="collapseFilter2Icon" class="fa fa-chevron-down"></i>
+                        <i id="collapseFilter3Icon" class="fa fa-chevron-down"></i>
                         By Price
                     </a>
                 </div>
@@ -67,10 +67,183 @@
                 </div>
             </form>
         </div>
+
+        <div class="filter-mobile" id="mobile">
+            <button class="btn" data-toggle="modal" data-target="#modal">Filter</button>
+        </div>
+
+        <div class="modal fade" id="modal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="filter-on-mobile">
+                            <form>
+                                <div class="filter-header1">
+                                    <a onclick="controlMobileFilter('collapseFilter1')" style="text-decoration: none;" data-toggle="collapse" href="#collapseFilter1">
+                                        <i id="collapseFilter1MobileIcon" class="fa fa-chevron-down"></i>
+                                        By Brand
+                                    </a>
+                                </div>
+                                <div class="filter-child collapse" id="collapseFilter1">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="check">
+                                        <label class="form-check-label" for="check">Nike</label>
+                                    </div>
+
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="check">
+                                        <label class="form-check-label" for="check">Adidas</label>
+                                    </div>
+                                </div>
+
+                                <div class="filter-header">
+                                    <a onclick="controlMobileFilter('collapseFilter2')" style="text-decoration: none;" data-toggle="collapse" href="#collapseFilter2">
+                                        <i id="collapseFilter2MobileIcon" class="fa fa-chevron-down"></i>
+                                        By Name
+                                    </a>
+                                </div>
+                                <div class="filter-child collapse" id="collapseFilter2">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="check">
+                                        <label class="form-check-label" for="check">Nike</label>
+                                    </div>
+
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="check">
+                                        <label class="form-check-label" for="check">Adidas</label>
+                                    </div>
+                                </div>
+
+                                <div class="filter-header">
+                                    <a onclick="controlMobileFilter('collapseFilter3')" style="text-decoration: none;" data-toggle="collapse" href="#collapseFilter3">
+                                        <i id="collapseFilter3MobileIcon" class="fa fa-chevron-down"></i>
+                                        By Price
+                                    </a>
+                                </div>
+                                <div class="filter-child collapse" id="collapseFilter3">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="check">
+                                        <label class="form-check-label" for="check">Ascending</label>
+                                    </div>
+
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="check">
+                                        <label class="form-check-label" for="check">Descending</label>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <a href="#" style="text-decoration: none; color: black">
+                        <div class="product">
+                            <div class="image-container">
+                                <img src="imgs/clothes-test.png">
+                            </div>
+                            <p>Women’s High-Rise Woven Pants
+                            <br>
+                            <span class="color">2 Colors</span>
+                            <br>
+                            70$
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col">
+                    <a href="#" style="text-decoration: none; color: black">
+                        <div class="product">
+                            <div class="image-container">
+                                <img src="imgs/clothes-test.png">
+                            </div>
+                            <p>
+                                Women’s High-Rise Woven Pants
+                                <br>
+                                <span class="color">2 Colors</span>
+                                <br>
+                                <span class="sale">
+                                    <span><span class="old-price">70$</span> 50$</span>
+                                    <button class="btn">On Sale</button>
+                                </span>
+                            </p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col">
+                    <a href="#" style="text-decoration: none; color: black">
+                        <div class="product">
+                            <div class="image-container">
+                                <img src="imgs/clothes-test.png">
+                            </div>
+                            <p>Women’s High-Rise Woven Pants
+                            <br>
+                            <span class="color">2 Colors</span>
+                            <br>
+                            70$
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col">
+                    <a href="#" style="text-decoration: none; color: black">
+                        <div class="product">
+                            <div class="image-container">
+                                <img src="imgs/clothes-test.png">
+                            </div>
+                            <p>Women’s High-Rise Woven Pants
+                            <br>
+                            <span class="color">2 Colors</span>
+                            <br>
+                            70$
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col">
+                    <a href="#" style="text-decoration: none; color: black">
+                        <div class="product">
+                            <div class="image-container">
+                                <img src="imgs/clothes-test.png">
+                            </div>
+                            <p>Women’s High-Rise Woven Pants
+                            <br>
+                            <span class="color">2 Colors</span>
+                            <br>
+                            70$
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col">
+                    <a href="#" style="text-decoration: none; color: black">
+                        <div class="product">
+                            <div class="image-container">
+                                <img src="imgs/clothes-test.png">
+                            </div>
+                            <p>Women’s High-Rise Woven Pants
+                            <br>
+                            <span class="color">2 Colors</span>
+                            <br>
+                            70$
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
     <?php include('footer.php') ?>
 </body>
-</html>
 
 <script>
     function controlFilter(id) {
@@ -80,4 +253,22 @@
             document.getElementById(id + 'Icon').className = "fa fa-chevron-down";
         }
     }
+
+    function controlMobileFilter(id) {
+        if($('#' + id).is('.collapse:not(.show)')) {
+            document.getElementById(id + 'MobileIcon').className = "fa fa-chevron-up";
+        } else if ($('#' + id).is('.collapse:not(.hidden)')) {
+            document.getElementById(id + 'MobileIcon').className = "fa fa-chevron-down";
+        }
+    }
+
+    $(window).resize(function() {
+        if ($(window).width() >= 700) {
+            if ($('#modal').hasClass('show')) {
+                $('#modal').modal('toggle');   
+            }
+        }
+    });
 </script>
+
+</html>
