489b356f8607a02c897f1a08523e9a3eff253c08
Ali Zein Al Dine <alized559@gmail.com>
2022-04-29 16:51:55 +0300

    .

--- panel.php
+++ panel.php
@@ -1,5 +1,15 @@
 <?php 
 include "includes/validateAuth.php";
+include 'includes/config.php';
+
+$id = $_SESSION['userID'];
+
+$user = $db -> query("SELECT * FROM users WHERE `user_id`='$id'");
+
+$fetch_check = $user->fetch_object();
+$fullname = $fetch_check->fullname;
+$username = $fetch_check->username;
+$email = $fetch_check->email;
 
 ?>
 
@@ -26,31 +36,53 @@ include "includes/validateAuth.php";
     </div>
 
     <div class="container-fluid">
-        <h3 class="user-title">John Mckay</h3>
+        <h3 class="user-title"><?php echo $fullname ?></h3>
         <div class="table-container">
             <table class="table borderless">
                 <thead>
                     <tr>
                         <th scope="col" class="title">Username</th>
-                        <th scope="col">@alized559</th>
+                        <th scope="col"><?php echo $username ?></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="col" class="title">Email Address</th>  
-                        <th scope="col">alized559@gmail.com</th>                              
+                        <th scope="col"><?php echo $email ?></th>                              
                     </tr>
                 </tbody>
             </table>
         </div>
 
         <button class="btn" style="margin-top: 10px; background-color: #EBEBEB; border-radius: 10px; width: 200px;">Manage Profile</button>
-    
+        <a href="logout.php">
+            <button class="btn" style="margin-top: 10px; background-color: #dc143c; color: white; border-radius: 10px; width: 100px;">Logout</button>
+        </a>
         <h3 class="favorites-title">Favorites</h3>
         <div class="favorite-products">
-            <img src='imgs/9.png' alt='Product Image'>
-            <img src='imgs/9.png' alt='Product Image'>
-            <img src='imgs/9.png' alt='Product Image'>
+            <?php
+                $favorites = $db -> query("SELECT prod_id FROM favorites WHERE `user_id`='$id'");
+                if(mysqli_num_rows($favorites) > 0) {
+                    while($row = mysqli_fetch_assoc($favorites)){
+                        
+                        $id = $row["prod_id"];
+                        $file = "images/products/" . $id . "/logo.jpg";
+
+                        if(!file_exists($file)){//Deletes the image if it exists
+                            $file = "images/products/" . $id . "/logo.png";
+                            if(!file_exists($file)){//Deletes the image if it exists
+                                $file = "images/products/" . $id . "/logo.gif";
+                                if(!file_exists($file)){//Deletes the image if it exists
+                                    $file = "images/products/default.png";
+                                }
+                            }
+                        }
+                        echo "<a href='viewProduct.php?id=$id'><img src='$file' alt='Product Image'></a>";
+                    }
+                } else {
+                    //no favorites
+                }
+            ?>
         </div>
 
         <h3 class="favorites-title">Previous Orders</h3>
@@ -135,10 +167,6 @@ include "includes/validateAuth.php";
             </div>
         </div>
     </div>
-
-    <!-- <div>
-        <a href="logout.php">Logout</a>
-    </div> -->
 
     <?php include('footer.php') ?>
 </body>
