daedf012dbd7c8cf4cac968e266db5fdcb8283af
Ali Zein Al Dine <alized559@gmail.com>
2022-05-09 17:05:41 +0300

    create help page

---
+++ help.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fashion | Help</title>
+
+    <link href="css/help.css" rel="stylesheet" media="screen">
+
+    <?php include('header.php') ?>
+</head>
+<body>
+    
+    <div class="help">
+        <div class="box">
+            <div class="left-side">
+                <h1>Support <br>and Help</h1>
+                <p>We’re always ready to answer questions about your orders and general questions about
+                    our products and services.</p>
+                <a href="mailto:info@fashion.com"><button class="btn btn-primary">Learn More</button></a>
+            </div>
+
+            <div class="right-side">
+                <img src="imgs/help2.png" alt="Help Image">
+            </div>
+        </div>
+    </div>
+
+    <?php include('footer.php') ?>
+</body>
+</html>
