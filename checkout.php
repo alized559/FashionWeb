edebcfc293524b546c082d96c5414356c6c3547e
Toxic Scams <toxicscams@gmail.com>
2022-05-10 20:16:12 +0300

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -65,7 +65,7 @@ if($totalCartItems == 0){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fashion | Checkout</title>
-
+    <link rel="icon" href="imgs/favicon.ico" type="image/x-icon">
     <link href="css/checkout.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
